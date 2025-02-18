f7a0f85580eb0db34353d452007dab5ed199c885
Brad Burch <bradburch@duck.com>
2025-02-17 17:41:51 -0800

    adding next js

---
+++ packages/client/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default async function Page() {
+  const data = await fetch('http://localhost:8080/api/jobs')
+  const jobs = await data.json()
+  return (
+    <ul>
+      {jobs.map((job) => (
+        <li key={job.id}>{job.title}</li>
+      ))}
+    </ul>
+  )
+}
